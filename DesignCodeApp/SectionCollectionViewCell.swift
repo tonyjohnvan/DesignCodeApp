6ea1367c7b26c201f6097d44aeb884c567ab629c
Fan Zhang <tonyjohnvan@users.noreply.github.com>
2018-09-13 13:25:57 -0400

    get data from static JSON

---
+++ DesignCodeApp/SectionCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SectionCollectionViewCell.swift
+//  DesignCodeApp
+//
+//  Created by Fan Zhang on 9/13/18.
+//  Copyright © 2018 Meng To. All rights reserved.
+//
+
+import UIKit
+
+class SectionCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var coverImageView: UIImageView!
+    
+}
