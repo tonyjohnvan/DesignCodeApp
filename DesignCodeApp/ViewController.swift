9476b3d76b1c32d610a2f3c07955527579f7f597
Fan Zhang <tonyjohnvan@users.noreply.github.com>
2018-09-12 22:08:07 -0400

    add all

--- DesignCodeApp/ViewController.swift
+++ DesignCodeApp/ViewController.swift
@@ -7,17 +7,39 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var deviceImage: UIImageView!
+    @IBOutlet weak var playVisualEffectView: UIVisualEffectView!
+    
+    @IBAction func playButtonTapped(_ sender: Any) {
+        let urlString = "https://player.vimeo.com/external/235468301.hd.mp4?s=e852004d6a46ce569fcf6ef02a7d291ea581358e&profile_id=175"
+        
+        let url = URL(string: urlString)
+        let player = AVPlayer(url: url!)
+        
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        
+        present(playerController,animated: true){
+            player.play()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        titleLabel.alpha = 0
+        deviceImage.alpha = 0
+        playVisualEffectView.alpha = 0
+        
+        UIView.animate(withDuration: 1) {
+            self.titleLabel.alpha = 1
+            self.deviceImage.alpha = 1
+            self.playVisualEffectView.alpha = 1
+        }
     }
 
 
