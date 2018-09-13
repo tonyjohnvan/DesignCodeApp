cd1a48d876273ba1b3227cb68794cd6a3e1df971
Fan Zhang <tonyjohnvan@gmail.com>
2018-09-13 18:01:07 -0400

    enhance skill pt1

--- DesignCodeApp/ViewController.swift
+++ DesignCodeApp/ViewController.swift
@@ -67,6 +67,7 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
         cell.titleLabel.text = section["title"]
         cell.captionLabel.text = section["caption"]
         cell.coverImageView.image = UIImage(named: section["image"]!)
+        cell.layer.transform = animateCell(cellFrame: cell.frame)
         return cell
     }
     
@@ -75,7 +76,6 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
 
 extension ViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-//        print(scrollView)
         let offsetY = scrollView.contentOffset.y
         if offsetY < 0 {
             heroView.transform = CGAffineTransform(translationX: 0, y: offsetY)
@@ -84,8 +84,51 @@ extension ViewController: UIScrollViewDelegate {
             titleLabel.transform = CGAffineTransform(translationX: 0, y: -offsetY/3)
             deviceImage.transform = CGAffineTransform(translationX: 0, y: -offsetY/4)
             backgroundImageView.transform = CGAffineTransform(translationX: 0, y: -offsetY/5)
-//            backgroundImageView.transform = CGAffineTransform(scaleX: 1+(-offsetY/500), y: 1+(-offsetY/500))
+            //backgroundImageView.transform = CGAffineTransform(scaleX: 1+(-offsetY/500), y: 1+(-offsetY/500))
         }
+        
+        if let collectionView = scrollView as? UICollectionView{
+            for cell in collectionView.visibleCells as! [SectionCollectionViewCell] {
+                let indexPath = collectionView.indexPath(for: cell)!
+                let attributes = collectionView.layoutAttributesForItem(at: indexPath)!
+                let cellFrame = collectionView.convert(attributes.frame, to: view)
+                
+                // animation starts here
+                let translationX = cellFrame.origin.x / 5
+                cell.coverImageView.transform = CGAffineTransform(translationX: translationX, y: 0)
+                
+                cell.layer.transform = animateCell(cellFrame: cellFrame)
+            }
+        }
+    }
+    
+    func animateCell(cellFrame: CGRect) -> CATransform3D {
+        
+        let angleFromX = Double((-cellFrame.origin.x)/10)
+        let angle = CGFloat((angleFromX * Double.pi)/180.0)
+        
+        var transform = CATransform3DIdentity
+        transform.m34 = -1.0/1000
+        
+        let rotation = CATransform3DRotate(transform, angle, 0, 1, 0)
+        //cell.layer.transform = rotation
+        
+        var scaleFromX = (1000 - (cellFrame.origin.x - 200)) / 1000
+        
+        let scaleMax: CGFloat = 1.0
+        let scaleMin: CGFloat = 0.6
+        
+        if scaleFromX > scaleMax{
+            scaleFromX = scaleMax
+        }
+        
+        if scaleFromX < scaleMin{
+            scaleFromX = scaleMin
+        }
+        
+        let scale = CATransform3DScale(CATransform3DIdentity, scaleFromX, scaleFromX, 1)
+        //cell.layer.transform = CATransform3DConcat(rotation, scale)
+        return CATransform3DConcat(rotation, scale)
     }
 }
 
